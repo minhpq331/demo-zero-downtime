a18258a7700faf89bba27553bf11a5c8b12963db
Minh Monmen <minhpq331@gmail.com>
2020-11-09 00:35:55 +0700

    update code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const os = require('os')
 const app = express()
 
 const port = parseInt(process.env.PORT, 10) || 0
-const message = process.env.MESSAGE
 
 const version = '2.0'
 
@@ -12,16 +11,12 @@ if (!port) {
 	process.exit(1)
 }
 
-if (!message) {
-	console.log('Missing env MESSAGE!')
-	process.exit(1)
-}
-
 app.get('/', (req, res) => {
   res.send(`Hello, I'm running on ${os.hostname()} with version ${version}!`)
-  res.send(`Message: ${message}`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at port ${port}`)
-})
+setTimeout(() => { 
+  app.listen(port, () => {
+    console.log(`Example app listening at port ${port}`)
+  })
+}, 5000);
